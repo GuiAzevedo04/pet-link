982de428a9e0274e63f6c45e9a7b17c34cd15b64
Mateus Mendes <matreuusmendes@gmail.com>
2025-01-20 13:08:52 -0300

    Repositorio Produto

---
+++ back-end/src/main/java/com/petlink/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.petlink.repository;
+
+import com.petlink.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
