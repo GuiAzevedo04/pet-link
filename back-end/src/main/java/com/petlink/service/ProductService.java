03a3df9ba88725196793ca6782483fbbfebc9f7a
Mateus Mendes <matreuusmendes@gmail.com>
2025-01-20 16:38:03 -0300

    Adicionando Service de Produto

---
+++ back-end/src/main/java/com/petlink/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.petlink.service;
+
+
+import com.petlink.data.dto.ProductResponseDTO;
+import com.petlink.data.entity.Product;
+import com.petlink.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductResponseDTO> getAllProducts(){
+        return productRepository.findAll().stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    public ProductResponseDTO createProduct(ProductResponseDTO productResponse){
+        Product produto = toEntity(productResponse);
+        Product saveProduct = productRepository.save(produto);
+
+        return toDto(saveProduct);
+
+    }
+
+
+    private ProductResponseDTO toDto(Product entity){
+        ProductResponseDTO product = new ProductResponseDTO();
+        product.setId(entity.getIdProduct());
+        product.setName(entity.getName());
+        product.setPrice(entity.getPrice());
+        product.setDescription(entity.getDescription());
+        product.setImageLink(entity.getImage_link());
+
+        return product;
+    }
+
+    private Product toEntity(ProductResponseDTO dto) {
+        Product product = new Product();
+        product.setIdProduct(dto.getId());
+        product.setName(dto.getName());
+        product.setPrice(dto.getPrice());
+        product.setDescription(dto.getDescription());
+        product.setImage_link(dto.getImageLink());
+        return product;
+    }
+}
