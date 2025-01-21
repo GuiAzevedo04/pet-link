9422f3b8c2a69e8c189954dd9fbf9e029e70950e
Mateus Mendes <matreuusmendes@gmail.com>
2025-01-21 09:00:25 -0300

    Error Handling e novas rotas em Controller

--- back-end/src/main/java/com/petlink/controller/UserController.java
+++ back-end/src/main/java/com/petlink/controller/UserController.java
@@ -9,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/cliente")
@@ -30,16 +31,41 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserResponseDTO userResponseDTO){
-        var emailPassword = new UsernamePasswordAuthenticationToken(userResponseDTO.getEmail(), userResponseDTO.getPassword());
-        var auth = this.authenticationManager.authenticate(emailPassword);
 
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+        try {
+            var emailPassword = new UsernamePasswordAuthenticationToken(userResponseDTO.getEmail(), userResponseDTO.getPassword());
+            var auth = this.authenticationManager.authenticate(emailPassword);
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of("token", token));
+        }
 
-        return ResponseEntity.status(HttpStatus.OK).body(token);
+        catch (BadCredentialsException exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "E-mail ou senha inválidos."));
+        }
+
+        catch (Exception exception){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Aconteceu um erro ao tentar realizar o login"));
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserResponseDTO userResponseDTO){
         return userService.register(userResponseDTO);
+    }
+
+    @DeleteMapping("delete/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long userId){
+        return userService.deleteUser(userId);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> profile(){
+        try {
+            User userLogged = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return ResponseEntity.status(HttpStatus.OK).body(userLogged);
+        }
+        catch (Exception exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "É preciso estar autenticado."));
+        }
     }
 }
