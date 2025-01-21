4c0c78dd4e3615d0cee8e9489bf8aa4453b6bcb2
Mateus Mendes <matreuusmendes@gmail.com>
2025-01-20 21:15:57 -0300

    Spring Security Configurações

---
+++ back-end/src/main/java/com/petlink/infra/Security/SecurityConfigurations.java
@@ -0,0 +1,42 @@
+package com.petlink.infra.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/cliente/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/cliente/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/produto/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/produto/**").hasRole("ADMIN"))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
