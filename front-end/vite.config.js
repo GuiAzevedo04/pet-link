c4cd1a9ab6db740083b6f0ada00803b8ee7d148c
leommelo <lmmelo096@gmail.com>
2025-01-21 18:56:21 -0300

    Tela de Produto Detalhado e carrinho feita

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://e3ho1fc6vuh8.share.zrok.io',
+        changeOrigin: true,
+        secure: false, // Ignorar problemas de SSL, se aplicável
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo "/api"
+      },
+    },
+  },
+});
